c4b8777a072e9aa0ab8a353cb7db8b1b42767667
AkinpeluOluwatayo <akinpeluoluwatayo1235@gmail.com>
2025-09-24 04:40:28 +0100

    practice

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Student s = new Student();
+        s.setName("Tayo");
+        s.setAge(29);
+
+        System.out.println(s.getName());
+        System.out.println(s.getAge());
+    }
+}
